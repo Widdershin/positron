6a7e03aa00f91cd7aea48ada661e0c8342a8e190
Hugh Davenport <hugh.davenport@powershop.co.nz>
2016-03-17 15:52:32 +1300

    Fix gnu-sedisms

--- bin/change-name.sh
+++ bin/change-name.sh
@@ -13,9 +13,9 @@ ORIGINAL_PACKAGE_REGEX="${ORIGINAL_PACKAGE//\./\\.}"
 
 cd "${DIRECTORY}"
 for FILE in app/{build.gradle,src/main/{AndroidManifest.xml,/java/${ORIGINAL_PACKAGE_PATH}/MainActivity.java,res/layout/activity_main.xml}}; do
-  sed -i "/${ORIGINAL_PACKAGE_REGEX}/s/${ORIGINAL_PACAKAGE_REGEX}/${NEW_PACKAGE//\//\\/}/" ${FILE}
+  sed -i -e "/${ORIGINAL_PACKAGE_REGEX}/s/${ORIGINAL_PACKAGE_REGEX}/${NEW_PACKAGE//\//\\/}/" ${FILE}
 done
-sed -i "/${ORIGINAL_TITLE}/s/${ORIGINAL_TITLE}/${NEW_TITLE//\//\\/}/" app/src/main/res/values/strings.xml
+sed -i -e "/${ORIGINAL_TITLE}/s/${ORIGINAL_TITLE}/${NEW_TITLE//\//\\/}/" app/src/main/res/values/strings.xml
 mkdir -p $(dirname app/src/main/java/${NEW_PACKAGE_PATH})
 mv app/src/main/java/{${ORIGINAL_PACKAGE_PATH},${NEW_PACKAGE_PATH}}
 cd - >/dev/null
